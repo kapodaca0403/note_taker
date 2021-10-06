83e43c3710764fbad0748a379bec6baee12dcb38
katerz avocado <ktrinaapodaca@yahoo.com>
2021-10-06 17:03:11 -0600

    adding changes

--- server.js
+++ server.js
@@ -1,100 +1,88 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const uuid = require("uuidv1");
+const uuid = require("./public/helpers/uuid");
 const util = require("util");
-const writeFileAsync = util.promisify(fs.writeFile);
-const readFileAsync = util.promisify(fs.readFile);
+
 const dbJson = require("./db/db.json");
 
 const app = express();
+
 const PORT = process.env.PORT || 3001;
+
+const readFromFile = util.promisify(fs.readFile);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// adding a status for how many notes have been added???
-let notes = [];
-
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/notes.htnl"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/notes.html"));
+  res.sendFile(path.join(__dirname, "public", "notes.html"));
 });
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "/public/index.html"));
-// });
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 app.get("/api/notes", (req, res) => {
-  console.log("testing this out ");
-  readFileAsync("./db/db.json", "utf8").then((notes) => {
-    console.log(notes);
-    const parsNotes = [].concat(JSON.parse(notes));
-    [].concat(JSON.parse(notes));
-    console.log(parsNotes);
-    res.json(parsNotes);
-    res.end();
-  });
+  console.info(`${req.method} request received for notes`);
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
-//app.get("/api/notes/:id", (req, res) => {
-//fs.readFile("./db/db.json", (err, notes)).then(notes => {
-//const parsNotes = JSON.parse(notes);
-//err ? console.error(err) : console.log(JSON.parse(notes));
-//});
-//res.json(notes);
-//});
-
-//app.get("/api/notes", (req, res) => {
-//console.info(`${req.method} request to add notes`);
-//return res.json(notes);
-//});
-app.post("/api/notes", (req, res) => {
-  const { title, text } = req.body;
-  //console.log(notes);
-  let addNote = { title, text, id: uuid() };
-  console.log(addNote);
-  //dbJson().push(addNote);
-  fs.writeFile(
-    "./db/db.json",
-    JSON.stringify(addNote),
-    function (err) {
-      res.end();
-    }
-
-    //err ? console.error(err) : console.log((addNote));
-    //});
-    //res.json(notes);
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
   );
-});
-// let newNote;
 
-//if (req.body && req.body.product) {
-//const response = {
-//status: "success",
-//data: req.body,
-//};
-//res.json(`Notes ${response.data.product} has been added`);
-//} else {
-//res.json("Please add context to notes");
-//}
-//console.log(req.body);
-//});
+const readAndAppend = (content, file) => {
+  fs.readFile(file, "utf8", (err, data) => {
+    console.log(data);
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      console.log(parsedData);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
 
-app.get("/api/notes/:id", (req, res) => {
-  res.json(notes[req.params.id]);
-});
+app.post("/api/notes", (req, res) => {
+  console.info(`${req.method} request received to add a tip`);
 
-app.delete("/api/notes/:id", (req, res) => {
-  const deleteNote = notes(req.params.id, notes);
-  if (deleteNote !== -1) {
-    notes.splice(deleteNote, 1);
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
+    };
+
+    readAndAppend(newNote, "./db/db.json");
+    res.json(`Note added`);
+  } else {
+    res.error("Error , please try adding note again");
   }
-  res.json(`Note has been deleted`);
 });
 
-app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
+// DELETE NOTE???
+// app.get("/api/notes/:id", (req, res) => {
+//   res.json(notes[req.params.id]);
+// });
+
+// app.delete("/api/notes/:id", (req, res) => {
+//   const deleteNote = notes(req.params.id, notes);
+//   if (deleteNote !== -1) {
+//     notes.splice(deleteNote, 1);
+//   }
+//   res.json(`Note has been deleted`);
+// });
+app.listen(PORT, () =>
+  console.log(`Listening to port http://localhost:${PORT}`)
+);
